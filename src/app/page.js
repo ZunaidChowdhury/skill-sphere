0144725000acf307e3520ff2d509c40b2af0c936
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-04-29 20:59:37 +0600

    project setup done

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-7xl'>Hello</h1>
+      <span className="loading loading-bars loading-xl"></span>
+    </div>
+  );
+}
