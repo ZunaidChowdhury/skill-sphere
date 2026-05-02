5bae74fbcf4c52b5c9fd2f70f755a8b84c1cd1cf
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-05-02 10:21:37 +0600

    ghost fetch on vercel, fix p2

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div>
       <Hero />
-      {/* <FeaturedCourses /> */}
+      <FeaturedCourses />
       <LearningTips />
       <TopInstructors />
       <WhyChooseUs />
